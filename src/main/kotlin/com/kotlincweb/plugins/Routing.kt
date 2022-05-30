97aeb453621f4ea0b63a06b3aaf98e37943fff8e
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2022-05-30 19:57:18 +0900

    Added index.html

--- src/main/kotlin/com/kotlincweb/plugins/Routing.kt
+++ src/main/kotlin/com/kotlincweb/plugins/Routing.kt
@@ -3,13 +3,19 @@ package com.kotlincweb.plugins
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 
 fun Application.configureRouting() {
 
     routing {
-        get("/") {
+        static("/"){
+            staticBasePackage = "static"
+            resource("index.html")
+            defaultResource("index.html")
+        }
+        get("/hello") {
             call.respondText("Hello World!")
         }
     }
