4d8516450d1d74399716284e7d5ba53ff2bea4b6
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2022-06-02 00:08:44 +0900

    run on digital ocean

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-call-logging:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-cors:$ktor_version")
--- src/main/kotlin/com/kotlincweb/Application.kt
+++ src/main/kotlin/com/kotlincweb/Application.kt
@@ -5,14 +5,15 @@ import com.kotlincweb.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.callloging.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureSerialization()
-        install(CORS) {
-            allowHost("lb-ktrueda.cloud.okteto.net")
-        }
+//        install(CORS) {
+//            anyHost()
+//        }
+        install(CallLogging)
     }.start(wait = true)
 }
